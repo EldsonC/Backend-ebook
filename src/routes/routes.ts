a000b0a5ecda51603a37d8a08e61b7246b4dab07
Eldson Caldas <81312380+EldsonC@users.noreply.github.com>
2023-07-27 14:22:34 -0300

    route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,9 @@
 import { Router } from "express";
-import { CreateBookController } from "../controllers/createBook";
-import { CreateCategoryController } from "../controllers/createCategory";
-import { GetBookController } from "../controllers/get-books";
-import { GetCategoriesController } from "../controllers/get-categories";
+import { GetBookController } from "../controllers/getBook";
+
 
 import multer from "multer";
 import { uploadImage } from "../services/firebase";
-import { CreateStudentsController } from "../controllers/createStudents";
-import { CreateBooksGetController } from "../controllers/createBooksGet";
 
 const router = Router()
 
@@ -15,14 +11,7 @@ const Multer = multer({
     storage: multer.memoryStorage(),
 })
 
-//create
-router.post("/create-book", Multer.single("image"), uploadImage, new CreateBookController().execute)
-router.post("/create-category", new CreateCategoryController().execute)
-router.post("/create-student", new CreateStudentsController().execute)
-router.post("/create-bookget", new CreateBooksGetController().execute)
-
 //get Data
-router.get("/get-books", new GetBookController().execute)
-router.get("/get-categories", new GetCategoriesController().execute)
+router.get("/get-book", new GetBookController().execute)
 
 export { router }
